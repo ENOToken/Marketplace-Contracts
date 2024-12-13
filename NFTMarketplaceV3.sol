e9a5e7b3d48f645cbe21af8275bc857ba0d4394c
delarosan1 <delarosan1@gmail.com>
2024-12-13 20:20:25 +0100

    v3.3 Marketplace

--- NFTMarketplaceV3.sol
+++ NFTMarketplaceV3.sol
@@ -677,21 +677,5 @@ contract NFTMarketplaceV3 is Initializable, ReentrancyGuardUpgradeable, Pausable
         return (highestOffer, offerIndex);
     }
 
-    /**
-     * @notice Cancela todas las ofertas existentes en el marketplace y devuelve los fondos
-     * @dev Esta función es solo para testing
-     */
-    function cancelAllOffers(
-        address[] calldata nftContracts,
-        uint256[] calldata tokenIds
-    ) external onlyOwner nonReentrant whenNotPaused {
-        if (nftContracts.length != tokenIds.length) revert("Arrays length mismatch");
-        
-        for (uint256 i = 0; i < nftContracts.length; i++) {
-            // Cancelamos todas las ofertas para cada NFT
-            _cancelAndRefundOffers(nftContracts[i], tokenIds[i], -1);
-        }
-    }
-
     receive() external payable {}
 }
